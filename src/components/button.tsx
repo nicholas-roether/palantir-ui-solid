48e8eadc981cb16a41ea22d3f80c38f6c2a952d3
Nicholas Roether <nicholas.roether@t-online.de>
2023-05-28 14:29:13 +0200

    Add link button support

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,19 +1,34 @@
 import { ComponentProps, JSX } from "solid-js";
+import { Dynamic } from "solid-js/web";
 
-interface ButtonProps extends ComponentProps<"button"> {
+interface BaseButtonProps {
 	large?: boolean;
 	smoldering?: boolean;
 }
 
+interface ActionButtonProps extends BaseButtonProps, ComponentProps<"button"> {
+	href?: undefined
+}
+
+interface LinkButtonProps extends BaseButtonProps, ComponentProps<"a"> {
+	href: string;
+}
+
+type ButtonProps = ActionButtonProps | LinkButtonProps;
+
 function Button({
 	large = false,
 	smoldering = false,
+	href,
 	classList,
 	...props
 }: ButtonProps): JSX.Element {
+	const component = href ? "a" : "button";
 	return (
-		<button
-			classList={{	
+		<Dynamic
+			component={component}
+			href={href}
+			classList={{
 				"pui-button": true,
 				"pui-large": large,
 				"pui-smoldering": smoldering,
